0d893065ebe27ed323c553b20b1be0b6430f491d
Evgeniy Schibrikov <shook@live.ru>
2018-12-16 00:01:49 +0500

    решение задачи

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -1,12 +1,41 @@
 import { IObservable, IObserver } from '../utils/observable/types';
 import { IView } from './types';
+import { NewsState } from '../state/news';
+import { WeatherState } from '../state/weather';
+import { IArticle } from '../state/news/types';
+import { IMeasurement } from '../state/weather/types';
 
 export class DesktopView implements IObserver, IView {
+    private dataSources: Set<IObservable> = new Set();
+
     public update(observable: IObservable) {
-        throw new Error('Not implemented');
+        this.dataSources.add(observable);
+        this.render();
     }
 
     public render() {
-        throw new Error('Not implemented');
+        const content: string[] = [];
+
+        this.dataSources.forEach(source => {
+            if (source instanceof NewsState) {
+                const lastArticles = source.getArticles().slice(-3);
+                content.push(...lastArticles.map(this.renderSingleArticle));
+            } else if (source instanceof WeatherState) {
+                const lastMeasurements = source.getMeasurements().slice(-2);
+                content.push(...lastMeasurements.map(this.renderSingleMeasurement));
+            }
+        });
+
+        console.log(`<div class="desktop">\n${content.join('\n')}\n</div>`);
+    }
+
+    private renderSingleArticle(news: IArticle) {
+        return `[${news.time}] ${news.category} - ${news.title}`;
+    }
+
+    private renderSingleMeasurement(measurement: IMeasurement) {
+        return `[${measurement.time}] ${measurement.temperature} C, ${measurement.pressure} P, ${
+            measurement.humidity
+        } U`;
     }
 }
--- src/views/mobile.ts
+++ src/views/mobile.ts
@@ -1,12 +1,45 @@
 import { IObservable, IObserver } from '../utils/observable/types';
 import { IView } from './types';
+import { NewsState } from '../state/news';
+import { IArticle } from '../state/news/types';
+import { IMeasurement } from '../state/weather/types';
+import { WeatherState } from '../state/weather';
 
 export class MobileView implements IObserver, IView {
+    private dataSources: Set<IObservable> = new Set();
+
     public update(observable: IObservable) {
-        throw new Error('Not implemented');
+        this.dataSources.add(observable);
+        this.render();
     }
 
     public render() {
-        throw new Error('Not implemented');
+        const content: string[] = [];
+
+        this.dataSources.forEach(source => {
+            if (source instanceof NewsState) {
+                const lastArticle = source.getArticles().slice(-1)[0];
+                if (lastArticle) {
+                    content.push(this.renderSingleArticle(lastArticle));
+                }
+            } else if (source instanceof WeatherState) {
+                const lastMeasurement = source.getMeasurements().slice(-1)[0];
+                if (lastMeasurement) {
+                    content.push(this.renderSingleMeasurement(lastMeasurement));
+                }
+            }
+        });
+
+        console.log(`<div class="mobile">\n${content.join('\n')}\n</div>`);
+    }
+
+    private renderSingleArticle(news: IArticle) {
+        return `[${news.time}] ${news.category} - ${news.title}`;
+    }
+
+    private renderSingleMeasurement(measurement: IMeasurement) {
+        return `[${measurement.time}] ${measurement.temperature} C, ${measurement.pressure} P, ${
+            measurement.humidity
+        } U`;
     }
 }
