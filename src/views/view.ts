8644a4d4dafd8de82b1e78c6a1cf8d43db47f2e3
epeshk <1197228@gmail.com>
2018-12-16 16:45:40 +0500

    try

--- src/views/view.ts
+++ src/views/view.ts
@@ -24,13 +24,14 @@ export abstract class View implements IObserver, IView {
         if (observable instanceof WeatherState) {
             const weatherState = observable as WeatherState;
             const measurements = weatherState.getMeasurements();
-            needRender = needRender || !this.equals(this.weather, measurements);
+            needRender =
+                needRender || !this.equals(this.weather, measurements, this.measurementEquals);
             this.weather = measurements;
         }
         if (observable instanceof NewsState) {
             const weatherState = observable as NewsState;
             const news = weatherState.getArticles();
-            needRender = needRender || !this.equals(this.news, news);
+            needRender = needRender || !this.equals(this.news, news, this.articleEquals);
             this.news = news;
         }
 
@@ -52,7 +53,20 @@ export abstract class View implements IObserver, IView {
         console.log(content);
     }
 
-    private equals<T>(arr1: T[], arr2: T[]): boolean {
-        return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
+    private equals<T>(arr1: T[], arr2: T[], cmp: (x: T, y: T) => boolean): boolean {
+        return arr1.length === arr2.length && arr1.every((v, i) => cmp(v, arr2[i]));
+    }
+
+    private measurementEquals(x: IMeasurement, y: IMeasurement): boolean {
+        return (
+            x.humidity === y.humidity &&
+            x.pressure === y.pressure &&
+            x.temperature === y.temperature &&
+            x.time === x.time
+        );
+    }
+
+    private articleEquals(x: IArticle, y: IArticle): boolean {
+        return x.category === y.category && x.time === y.time && x.title === y.title;
     }
 }
