6a5e408f9847930186e1b8d3ea7fb4f6a62cc9a9
Drusalyator <And-gridin@yandex.ru>
2018-12-16 05:46:05 +0500

    clear code

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -1,47 +1,10 @@
-import { IObservable, IObserver } from '../utils/observable/types';
-import { IView } from './types';
-import { NewsState } from '../state/news';
-import { WeatherState } from '../state/weather';
-import { IArticle } from '../state/news/types';
-import { IMeasurement } from '../state/weather/types';
-import { Render } from '../render/index';
+import { Device } from './device';
 
-export class DesktopView implements IObserver, IView {
-    private articleCount: number = 3;
-    private weatherCount: number = 2;
-    private htmlClass: string = 'desktop';
-    private articles: IArticle[] = [];
-    private measurements: IMeasurement[] = [];
-    private markup: string = '';
-
-    public update(observable: IObservable) {
-        if (observable instanceof NewsState) {
-            const rawArticles = observable.getArticles();
-            this.articles = rawArticles.slice(rawArticles.length - this.articleCount);
-        } else if (observable instanceof WeatherState) {
-            const rawMeasurements = observable.getMeasurements();
-            this.measurements = rawMeasurements.slice(rawMeasurements.length - this.weatherCount);
-        } else {
-            throw new TypeError();
-        }
-
-        const newMarkup = Render.createView(this.articles, this.measurements, this.htmlClass);
-
-        if (this.isNeedRender(newMarkup)) {
-            this.markup = newMarkup;
-            this.render();
-        }
-    }
-
-    public render() {
-        console.log(this.markup);
-    }
-
-    private isNeedRender(markup: string) {
-        if (this.markup !== markup || this.markup === '') {
-            return true;
-        }
-
-        return false;
+export class DesktopView extends Device {
+    constructor() {
+        super();
+        this.htmlClass = 'desktop';
+        this.articleCount = 3;
+        this.weatherCount = 2;
     }
 }
--- src/views/mobile.ts
+++ src/views/mobile.ts
@@ -1,47 +1,10 @@
-import { IObservable, IObserver } from '../utils/observable/types';
-import { IView } from './types';
-import { NewsState } from '../state/news';
-import { WeatherState } from '../state/weather';
-import { IArticle } from '../state/news/types';
-import { IMeasurement } from '../state/weather/types';
-import { Render } from '../render/index';
+import { Device } from './device';
 
-export class MobileView implements IObserver, IView {
-    private articleCount: number = 1;
-    private weatherCount: number = 1;
-    private htmlClass: string = 'mobile';
-    private articles: IArticle[] = [];
-    private measurements: IMeasurement[] = [];
-    private markup: string = '';
-
-    public update(observable: IObservable) {
-        if (observable instanceof NewsState) {
-            const rawArticles = observable.getArticles();
-            this.articles = rawArticles.slice(rawArticles.length - this.articleCount);
-        } else if (observable instanceof WeatherState) {
-            const rawMeasurements = observable.getMeasurements();
-            this.measurements = rawMeasurements.slice(rawMeasurements.length - this.weatherCount);
-        } else {
-            throw new TypeError();
-        }
-
-        const newMarkup = Render.createView(this.articles, this.measurements, this.htmlClass);
-
-        if (this.isNeedRender(newMarkup)) {
-            this.markup = newMarkup;
-            this.render();
-        }
-    }
-
-    public render() {
-        console.log(this.markup);
-    }
-
-    private isNeedRender(markup: string) {
-        if (this.markup !== markup || this.markup === '') {
-            return true;
-        }
-
-        return false;
+export class MobileView extends Device {
+    constructor() {
+        super();
+        this.htmlClass = 'mobile';
+        this.articleCount = 1;
+        this.weatherCount = 1;
     }
 }
