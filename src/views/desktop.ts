b510487c828e1c77a11b2013740fe0126bd0a7c1
Drusalyator <And-gridin@yandex.ru>
2018-12-16 05:15:41 +0500

    try clear

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -5,6 +5,7 @@ import { WeatherState } from '../state/weather';
 import { IArticle } from '../state/news/types';
 import { IMeasurement } from '../state/weather/types';
 import { Render } from '../render/index';
+import { getInfo } from './helper';
 
 export class DesktopView implements IObserver, IView {
     private articleCount: number = 3;
@@ -15,15 +16,14 @@ export class DesktopView implements IObserver, IView {
     private markup: string = '';
 
     public update(observable: IObservable) {
-        if (observable instanceof NewsState) {
-            const rawArticles = observable.getArticles();
-            this.articles = rawArticles.slice(rawArticles.length - this.articleCount);
-        } else if (observable instanceof WeatherState) {
-            const rawMeasurements = observable.getMeasurements();
-            this.measurements = rawMeasurements.slice(rawMeasurements.length - this.weatherCount);
-        } else {
-            throw new TypeError();
-        }
+        const { articles, measurements } = getInfo(
+            observable,
+            this.articleCount,
+            this.weatherCount
+        );
+
+        this.articles = articles;
+        this.measurements = measurements;
 
         this.markup = Render.createView(this.articles, this.measurements, this.htmlClass);
         this.render();
--- src/views/helper.ts
+++ src/views/helper.ts
@@ -3,32 +3,19 @@ import { NewsState } from '../state/news';
 import { WeatherState } from '../state/weather';
 import { IArticle } from '../state/news/types';
 import { IMeasurement } from '../state/weather/types';
-import { Render } from '../render/index';
 
-function getInformation(observable: IObservable) {
+export function getInfo(observable: IObservable, articleCount: number, weatherCount: number) {
     let articles: IArticle[] = [];
     let measurements: IMeasurement[] = [];
-
     if (observable instanceof NewsState) {
-        articles = observable.getArticles();
+        const rawArticles = observable.getArticles();
+        articles = rawArticles.slice(rawArticles.length - articleCount);
     } else if (observable instanceof WeatherState) {
-        measurements = observable.getMeasurements();
+        const rawMeasurements = observable.getMeasurements();
+        measurements = rawMeasurements.slice(rawMeasurements.length - weatherCount);
     } else {
         throw new TypeError();
     }
 
     return { articles, measurements };
-}
-
-export function createResultView(
-    observable: IObservable,
-    articleCount: number,
-    weatherCount: number,
-    htmlClass: string
-): string {
-    const { articles, measurements } = getInformation(observable);
-    const resultArticles = articles.slice(articles.length - articleCount);
-    const resultMeasurements = measurements.slice(measurements.length - weatherCount);
-
-    return Render.createView(resultArticles, resultMeasurements, htmlClass);
 }
