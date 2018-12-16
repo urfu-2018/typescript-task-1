08df50816995c3277200a4493e6e442ea044be15
Drusalyator <And-gridin@yandex.ru>
2018-12-17 03:56:21 +0500

    good compare

--- src/views/device.ts
+++ src/views/device.ts
@@ -12,36 +12,40 @@ export class Device implements IObserver, IView {
     protected htmlClass: string = 'device';
     private articles: IArticle[] = [];
     private measurements: IMeasurement[] = [];
-    private markup: string = '';
 
     public update(observable: IObservable) {
         if (observable instanceof NewsState) {
+            const lastArticles = this.articles;
             const rawArticles = observable.getArticles();
             this.articles = rawArticles.slice(rawArticles.length - this.articleCount);
+            if (this.isNeedRender(lastArticles, this.articles)) {
+                this.render();
+            }
         } else if (observable instanceof WeatherState) {
+            const lastMeasurements = this.measurements;
             const rawMeasurements = observable.getMeasurements();
             this.measurements = rawMeasurements.slice(rawMeasurements.length - this.weatherCount);
+            if (this.isNeedRender(lastMeasurements, this.measurements)) {
+                this.render();
+            }
         } else {
             throw new TypeError();
-        }
-
-        const newMarkup = Render.createView(this.articles, this.measurements, this.htmlClass);
-
-        if (this.isNeedRender(newMarkup)) {
-            this.markup = newMarkup;
-            this.render();
         }
     }
 
     public render() {
-        console.log(this.markup);
+        const markup = Render.createView(this.articles, this.measurements, this.htmlClass);
+        console.log(markup);
     }
 
-    private isNeedRender(markup: string) {
-        if (this.markup !== markup || this.markup === '') {
-            return true;
+    private isNeedRender<T>(oldCollection: T[], newCollection: T[]): boolean {
+        if (
+            oldCollection.length === newCollection.length &&
+            oldCollection.every((value, index) => value === newCollection[index])
+        ) {
+            return false;
         }
 
-        return false;
+        return true;
     }
 }
