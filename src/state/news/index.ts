da00deed70c24eba95924028679d2c33be18ab0a
Resinchen <alex1410alex@gmail.com>
2018-12-16 20:10:23 +0500

    Решение

--- src/state/news/index.ts
+++ src/state/news/index.ts
@@ -2,11 +2,14 @@ import { Observable } from '../../utils/observable';
 import { IArticle, INewsState } from './types';
 
 export class NewsState extends Observable implements INewsState {
+    private articles: IArticle[] = [];
+
     public getArticles() {
-        return [];
+        return JSON.parse(JSON.stringify(this.articles));
     }
 
     public setArticles(articles: IArticle[]) {
-        throw new Error('Not implemented');
+        this.articles = articles;
+        this.notifyObservers();
     }
 }
--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -1,12 +1,59 @@
 import { IObservable, IObserver } from '../utils/observable/types';
 import { IView } from './types';
+import { NewsState } from '../state/news';
+import { WeatherState } from '../state/weather';
+import { IArticle } from '../state/news/types';
+import { IMeasurement } from '../state/weather/types';
 
 export class DesktopView implements IObserver, IView {
+    private static articleCount: number = 3;
+    private static measurementsCount: number = 2;
+
+    private static prepare(measurements: IMeasurement[], articles: IArticle[]): string {
+        let result: string = '';
+
+        result = articles.reduce((value, article) => {
+            const { time, category, title } = article;
+            return value + `[${time}] ${category} - ${title}\n`;
+        }, result);
+
+        result = measurements.reduce((value, measurement) => {
+            const { time, pressure, humidity, temperature } = measurement;
+            return value + `[${time}] ${temperature} C, ${pressure} P, ${humidity} U\n`;
+        }, result);
+
+        return `<div class="desktop">\n${result}</div>`;
+    }
+
+    private markup: string = '';
+    private lastMarkup: string | undefined;
+    private lastMeasurements: IMeasurement[] = [];
+    private lastArticles: IArticle[] = [];
+
     public update(observable: IObservable) {
-        throw new Error('Not implemented');
+        if (observable instanceof NewsState) {
+            const articles = observable.getArticles();
+
+            this.lastArticles = articles.slice(articles.length - DesktopView.articleCount);
+        } else if (observable instanceof WeatherState) {
+            const measurements = observable.getMeasurements();
+
+            this.lastMeasurements = measurements.slice(
+                measurements.length - DesktopView.measurementsCount
+            );
+        } else {
+            return;
+        }
+
+        this.markup = DesktopView.prepare(this.lastMeasurements, this.lastArticles);
+
+        if (!this.lastMarkup || this.lastMarkup !== this.markup) {
+            this.render();
+            this.lastMarkup = this.markup;
+        }
     }
 
     public render() {
-        throw new Error('Not implemented');
+        console.log(this.markup);
     }
 }
