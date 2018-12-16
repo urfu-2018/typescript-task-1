d536c9576d5fb688bb67f407de9a01ac2aed0088
phil9l <phil9lne@gmail.com>
2018-12-16 17:17:12 +0500

    Init ts

--- src/views/mobile.ts
+++ src/views/mobile.ts
@@ -1,12 +1,7 @@
-import { IObservable, IObserver } from '../utils/observable/types';
-import { IView } from './types';
+import { BaseView } from './base';
 
-export class MobileView implements IObserver, IView {
-    public update(observable: IObservable) {
-        throw new Error('Not implemented');
-    }
-
-    public render() {
-        throw new Error('Not implemented');
-    }
+export class MobileView extends BaseView {
+    protected articlesCount: number = 1;
+    protected measurementsCount: number = 1;
+    protected typeName: string = 'mobile';
 }
