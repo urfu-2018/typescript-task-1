83746ab950ec80412318522e8d7cde2288e73306
kartsev <kartsev@skbkontur.ru>
2018-12-18 18:37:57 +0500

    add additional task

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -4,33 +4,39 @@ import { NewsState } from '../state/news';
 import { WeatherState } from '../state/weather';
 
 export class DesktopView implements IObserver, IView {
-    private lastArticle: string;
-    private lastMeasurement: string;
+    private lastArticles: string;
+    private lastMeasurements: string;
 
     constructor() {
-        this.lastArticle = '';
-        this.lastMeasurement = '';
+        this.lastArticles = '';
+        this.lastMeasurements = '';
     }
 
     public update(observable: IObservable) {
+        const oldArticles = this.lastArticles.slice(0);
+        const oldMeasurements = this.lastMeasurements.slice(0);
         if (observable instanceof NewsState) {
             const articles = (observable as NewsState).getArticles().slice(-3);
-            this.lastArticle = articles.reduce(
+            this.lastArticles = articles.reduce(
                 (x, y) => x + `[${y.time}] ${y.category} - ${y.title}\n`,
                 ''
             );
         }
         if (observable instanceof WeatherState) {
             const measurements = (observable as WeatherState).getMeasurements().slice(-2);
-            this.lastMeasurement = measurements.reduce(
+            this.lastMeasurements = measurements.reduce(
                 (x, y) => x + `[${y.time}] ${y.temperature} C, ${y.pressure} P, ${y.humidity} U\n`,
                 ''
             );
         }
-        this.render();
+        if (oldArticles !== this.lastArticles || oldMeasurements !== this.lastMeasurements) {
+            this.render();
+        }
     }
 
     public render() {
-        console.log('<div class="desktop">\n' + this.lastArticle + this.lastMeasurement + '</div>');
+        console.log(
+            '<div class="desktop">\n' + this.lastArticles + this.lastMeasurements + '</div>'
+        );
     }
 }
