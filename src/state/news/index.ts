3b9c9a22c5be8721d7bc5d1eb05c0f64f9fdcfd2
Afanasev Leonid <leo-6809@mail.ru>
2018-12-18 19:11:40 +0500

    Моё решение задачи

--- src/state/news/index.ts
+++ src/state/news/index.ts
@@ -2,14 +2,14 @@ import { Observable } from '../../utils/observable';
 import { IArticle, INewsState } from './types';
 
 export class NewsState extends Observable implements INewsState {
-    private articles: IArticle[]=[];
+    private articles: IArticle[] = [];
 
     public getArticles() {
         return this.articles;
     }
 
     public setArticles(articles: IArticle[]) {
-        this.articles=articles;
+        this.articles = articles;
         this.notifyObservers();
     }
 }
--- src/state/weather/index.ts
+++ src/state/weather/index.ts
@@ -2,14 +2,14 @@ import { IMeasurement, IWeatherState } from './types';
 import { Observable } from '../../utils/observable';
 
 export class WeatherState extends Observable implements IWeatherState {
-    private measurements:IMeasurement[]=[];
+    private measurements: IMeasurement[] = [];
 
     public getMeasurements() {
         return this.measurements;
     }
 
     public setMeasurements(measurements: IMeasurement[]) {
-        this.measurements=measurements;
+        this.measurements = measurements;
         this.notifyObservers();
     }
 }
