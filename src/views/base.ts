eb6bebf980fe902b53d0648a996b1f95939627b1
Afanasev Leonid <leo-6809@mail.ru>
2018-12-18 19:09:55 +0500

    Моё решение задачи

--- src/views/base.ts
+++ src/views/base.ts
@@ -1,3 +1,46 @@
-export interface IView {
-    render(): void;
+import { IObservable, IObserver } from '../utils/observable/types';
+import { IView } from './types';
+import { NewsState } from '../state/news/index';
+import { IArticle } from '../state/news/types';
+import { WeatherState } from '../state/weather/index';
+import { IMeasurement } from '../state/weather/types';
+
+export abstract class BaseView implements IObserver, IView {
+    protected abstract articlesCount: number;
+    protected abstract measurementsCount: number;
+    protected abstract typeName: string;
+
+    private previousMarkup: string = '';
+    private articles: IArticle[] = [];
+    private measurements: IMeasurement[] = [];
+
+    private static renderArticle(item: IArticle) {
+        return `[${item.time}] ${item.category} - ${item.title}`;
+    }
+
+    private static renderMeasurement(item: IMeasurement) {
+        return `[${item.time}] ${item.temperature} C, ${item.pressure} P, ${item.humidity} U`;
+    }
+
+    public update(observable: IObservable) {
+        if (observable instanceof NewsState) {
+            this.articles = observable.getArticles().slice(-this.articlesCount);
+        } else if (observable instanceof WeatherState) {
+            this.measurements = observable.getMeasurements().slice(-this.measurementsCount);
+        } else {
+            throw TypeError();
+        }
+        this.render();
+    }
+
+    public render() {
+        const articles = this.articles.map(BaseView.renderArticle);
+        const measurements = this.measurements.map(BaseView.renderMeasurement);
+        const reports = articles.concat(measurements).join('\n');
+        const markup = `<div class="${this.typeName}">\n${reports}\n</div>`;
+        if (this.previousMarkup !== markup) {
+            console.log(markup);
+            this.previousMarkup = markup;
+        }
+    }
 }
