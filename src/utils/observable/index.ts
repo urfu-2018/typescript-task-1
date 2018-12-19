48a072f269ac519f540fe780cde06a73f4ed8ecc
Leoltron <leoole5@gmail.com>
2018-12-20 01:44:47 +0500

    Fixed deleteObserver

--- src/utils/observable/index.ts
+++ src/utils/observable/index.ts
@@ -8,12 +8,19 @@ export class Observable implements IObservable {
     }
 
     public deleteObserver(observer: IObserver) {
-        this.observers.unshift(observer);
+        removeElement(this.observers, observer);
     }
 
     public notifyObservers() {
         for (const observer of this.observers) {
             observer.update(this);
         }
+    }
+}
+
+function removeElement<T>(array: T[], element: T): void {
+    const index = array.indexOf(element);
+    if (index > -1) {
+        array.splice(index, 1);
     }
 }
