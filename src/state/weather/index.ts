0972de1fd989f21b22a935adfaeaf48eb32d5439
elena-i-m98 <vesennyaja2016@yandex.ru>
2018-12-20 10:57:59 +0500

    пишем новости

--- src/state/weather/index.ts
+++ src/state/weather/index.ts
@@ -2,7 +2,7 @@ import { IMeasurement, IWeatherState } from './types';
 import { Observable } from '../../utils/observable';
 
 export class WeatherState extends Observable implements IWeatherState {
-    private measurments: IMeasurement[] = []
+    private measurments: IMeasurement[] = [];
     public getMeasurements() {
         return this.measurments;
     }
