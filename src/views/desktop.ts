b868c9048472cfe1705620bfe6720ad67965948d
jellymary <marykuzovleva@gmail.com>
2018-12-21 17:06:31 +0500

    refactoring again

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -2,19 +2,6 @@ import { View } from './types';
 
 export class DesktopView extends View {
     public render() {
-        this.setLastNews(3, 2);
-        let message = `<div class="desktop">\n`;
-        this.states.news.forEach(
-            news => (message += `[${news.time}] ${news.category} - ${news.title}\n`)
-        );
-        this.states.weather.forEach(
-            weather =>
-                (message += `[${weather.time}] ${weather.temperature} C, ${weather.pressure} P, ${
-                    weather.humidity
-                } U\n`)
-        );
-        message += `</div>`;
-
-        console.log(message);
+        console.log(this.renderFormat('desktop', 3, 2));
     }
 }
--- src/views/mobile.ts
+++ src/views/mobile.ts
@@ -2,20 +2,6 @@ import { View } from './types';
 
 export class MobileView extends View {
     public render() {
-        this.setLastNews(1, 1);
-        let message = `<div class="mobile">\n`;
-        this.states.news.forEach(
-            news => (message += `[${news.time}] ${news.category} - ${news.title}\n`)
-        );
-
-        this.states.weather.forEach(
-            weather =>
-                (message += `[${weather.time}] ${weather.temperature} C, ${weather.pressure} P, ${
-                    weather.humidity
-                } U\n`)
-        );
-        message += `</div>`;
-
-        console.log(message);
+        console.log(this.renderFormat('mobile', 1, 1));
     }
 }
