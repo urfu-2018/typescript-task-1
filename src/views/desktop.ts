66f408f502d85d2bd8c0a07b02a8c79ce34af819
Azakov <azakov31@gmail.com>
2018-12-21 11:39:28 +0500

    first try

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -1,12 +1,7 @@
-import { IObservable, IObserver } from '../utils/observable/types';
-import { IView } from './types';
+import { Views } from './views';
 
-export class DesktopView implements IObserver, IView {
-    public update(observable: IObservable) {
-        throw new Error('Not implemented');
-    }
-
-    public render() {
-        throw new Error('Not implemented');
+export class DesktopView extends Views {
+    constructor() {
+        super('desktop', 3, 2);
     }
 }
---
+++ src/views/views.ts
@@ -0,0 +1,53 @@
+import { IObservable, IObserver } from '../utils/observable/types';
+import { IView } from './types';
+import { IArticle } from '../state/news/types';
+import { IMeasurement } from '../state/weather/types';
+import { WeatherState } from '../state/weather';
+import { NewsState } from '../state/news';
+
+export class Views implements IObserver, IView {
+    private articleCount = 0;
+    private measurementsCount = 0;
+    private deviceName = '';
+    private article: IArticle[] = [];
+    private measurements: IMeasurement[] = [];
+    private resultMarkup = '';
+
+    protected constructor(deviceType: string, articleCount: number, measurementsCount: number) {
+        this.deviceName = deviceType;
+        this.articleCount = articleCount;
+        this.measurementsCount = measurementsCount;
+    }
+
+    public update(observable: IObservable): void {
+        if (observable instanceof WeatherState) {
+            const gotMeasur = observable.getMeasurements();
+            this.measurements = gotMeasur.slice(gotMeasur.length - this.measurementsCount);
+        } else if (observable instanceof NewsState) {
+            const gotArticles = observable.getArticles();
+            this.article = gotArticles.slice(gotArticles.length - this.articleCount);
+        } else {
+            throw new TypeError();
+        }
+
+        this.render();
+    }
+
+    public render() {
+        const renderNews = this.article
+            .map(art => `[${art.time}]${art.category} - ${art.title}\n`)
+            .join('');
+        const renderMeasur = this.measurements
+            .map(
+                meas =>
+                    `[${meas.time}] ${meas.temperature} C,` +
+                    `${meas.pressure} P, ${meas.humidity} U\n`
+            )
+            .join('');
+        const markup = `<div class="${this.deviceName}">\n${renderNews}${renderMeasur}</div>`;
+        if (markup !== this.resultMarkup) {
+            this.resultMarkup = markup;
+            console.log(this.resultMarkup);
+        }
+    }
+}
