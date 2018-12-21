d8078e8014379abec52419df6ba05a7ffd50c40a
Aleksey <AllEXEcuted>
2018-12-21 17:30:10 +0500

    test

---
+++ src/views/view.ts
@@ -0,0 +1,60 @@
+import { IObservable, IObserver } from '../utils/observable/types';
+import { IView } from './types';
+import { WeatherState } from '../state/weather';
+import { IMeasurement } from '../state/weather/types';
+import { NewsState } from '../state/news';
+import { IArticle } from '../state/news/types';
+
+export abstract class View implements IObserver, IView {
+    public readonly viewType: string;
+    private weather: IMeasurement[] = [];
+    private news: IArticle[] = [];
+    private countNews: number;
+    private countMeasurements: number;
+    private rendition: string | undefined;
+
+    protected constructor(countNews: number, countMeasurements: number, viewType: string) {
+        this.countNews = countNews;
+        this.countMeasurements = countMeasurements;
+        this.viewType = viewType;
+    }
+
+    public update(observable: IObservable) {
+        if (observable instanceof WeatherState) {
+            this.weather = (observable as WeatherState).getMeasurements();
+        } else if (observable instanceof NewsState) {
+            this.news = (observable as NewsState).getArticles();
+        }
+        this.render();
+    }
+
+    public render() {
+        const rendition = `<div class="${
+            this.viewType
+        }">\n${this.weatherToStr()}${this.newsToStr()}</div>`;
+
+        if (rendition !== this.rendition) {
+            console.log(rendition);
+            this.rendition = rendition;
+        }
+    }
+
+    private weatherToStr(): string {
+        return this.weather
+            .slice(-this.countMeasurements)
+            .map(
+                weather =>
+                    `[${weather.time}] ${weather.temperature} C, ${weather.pressure} P, ${
+                        weather.humidity
+                    } U\n`
+            )
+            .join('');
+    }
+
+    private newsToStr(): string {
+        return this.news
+            .slice(-this.countNews)
+            .map(nextNew => `[${nextNew.time}] ${nextNew.category} - ${nextNew.title}\n`)
+            .join('');
+    }
+}
