6fc65a7ed25d2231f994ecd9f158d0e816486c9b
elena-i-m98 <vesennyaja2016@yandex.ru>
2018-12-21 17:41:17 +0500

    предположительно готово

--- src/views/mobile.ts
+++ src/views/mobile.ts
@@ -1,12 +1,33 @@
 import { IObservable, IObserver } from '../utils/observable/types';
 import { IView } from './types';
+import { IArticle } from '../state/news/types';
+import { IMeasurement } from '../state/weather/types';
+import { WeatherState } from '../state/weather';
+import { NewsState } from '../state/news';
 
 export class MobileView implements IObserver, IView {
+    private articles: IArticle[] = [];
+    private measurements: IMeasurement[] = [];
+
     public update(observable: IObservable) {
-        throw new Error('Not implemented');
+        if (observable instanceof WeatherState) {
+            this.measurements = observable.getMeasurements().slice(-1);
+            this.render();
+        }
+        else if (observable instanceof NewsState) {
+            this.articles = observable.getArticles().slice(-1);
+            this.render();
+        }
     }
 
     public render() {
-        throw new Error('Not implemented');
+        const newContent = [
+            ...this.measurements.map(measurement => 
+                `[${measurement.time}] ${measurement.temperature} C, ${measurement.pressure} P, ${measurement.humidity} U`
+            ), 
+            ...this.articles.map(article => `[${article.time}] ${article.category} - ${article.title}`
+            )
+        ];
+        console.log(`<div class="mobile">\n${newContent.join('\n')}\n</div>`);
     }
 }
