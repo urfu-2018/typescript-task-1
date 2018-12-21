048e3a66b708d834781d0dc5c73a97d29ce6a9e2
creewick <ilia_Y@mail.ru>
2018-12-21 16:36:30 +0500

    мьех..

--- src/utils/observable/index.ts
+++ src/utils/observable/index.ts
@@ -1,17 +1,14 @@
 import { IObservable, IObserver } from './types';
 
 export class Observable implements IObservable {
-    private readonly observers: IObserver[] = new Array();
+    private readonly observers: Set<IObserver> = new Set<IObserver>();
 
     public addObserver(observer: IObserver) {
-        this.observers.push(observer);
+        this.observers.add(observer);
     }
 
     public deleteObserver(observer: IObserver) {
-        const i = this.observers.indexOf(observer);
-        if (i !== -1) {
-            this.observers.splice(i, 1);
-        }
+        this.observers.delete(observer);
     }
 
     public notifyObservers() {
