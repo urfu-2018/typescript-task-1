6dc77452b976e800e7a6fe6af33cb8856ecb9760
Timofey Utrobin <timofeyutrobin@gmail.com>
2018-12-22 15:58:00 +0500

    forgot to delete comments

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -1,7 +1,3 @@
-// import { IObservable, IObserver } from '../utils/observable/types';
-// import { WeatherState } from '../state/weather/index';
-// import { NewsState } from '../state/news/index';
-// import { IView } from './types';
 import { View } from './view';
 
 export class DesktopView extends View {
--- src/views/mobile.ts
+++ src/views/mobile.ts
@@ -1,5 +1,3 @@
-// import { IObservable, IObserver } from '../utils/observable/types';
-// import { IView } from './types';
 import { View } from './view';
 
 export class MobileView extends View {
