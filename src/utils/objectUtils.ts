1e4d32b7a10cbbd77081d304670a77ea84651513
Shchupakov Antony <a.shchupakov@mail.ru>
2018-12-28 02:49:58 +0500

    функция isEqual стала универсальной

--- src/utils/objectUtils.ts
+++ src/utils/objectUtils.ts
@@ -1,5 +1,29 @@
 export function isEqual<T extends object>(a: T, b: T): boolean {
-    return false;
+    if (typeof a !== 'object') {
+        return a === b;
+    } else if (a instanceof Array && b instanceof Array) {
+        if (a.length !== b.length) {
+            return false;
+        }
+        for (let i = 0; i < a.length; i++) {
+            if (!isEqual(a[i], b[i])) {
+                return false;
+            }
+        }
+    } else {
+        const keys = Reflect.ownKeys(a);
+        for (const key of keys) {
+            if (b.hasOwnProperty(key)) {
+                if (!isEqual((a as any)[key], (b as any)[key])) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 export function deepCopy<T extends object>(a: T): T {
