0c6ec7edc2e68dc16a9e0c601b0696265e861598
Shchupakov Antony <a.shchupakov@mail.ru>
2019-01-06 23:03:57 +0500

    Новости и погода формируются независимо друг от друга

--- src/utils/viewUtils.ts
+++ src/utils/viewUtils.ts
@@ -5,29 +5,40 @@ import { IObservable } from './observable/types';
 import { NewsState } from '../state/news';
 import { WeatherState } from '../state/weather';
 import { isEqual } from './objectUtils';
+import { ViewElements } from '../state/types';
 
-function prepareView(
-    htmlClass: string,
-    articles: IArticle[],
-    measurements: IMeasurement[]
-): string {
-    let result = '';
-
-    result = articles.reduce((prevValue, article) => {
-        const { time, category, title } = article;
-        return prevValue + `[${time}] ${category} - ${title}\n`;
-    }, result);
-
-    result = measurements.reduce((prevValue, measurement) => {
-        const { time, pressure, humidity, temperature } = measurement;
-        return prevValue + `[${time}] ${temperature} C, ${pressure} P, ${humidity} U\n`;
-    }, result);
-
-    return `<div class="${htmlClass}">\n${result}</div>`;
+function prepareViewToRender(htmlClass: string, articles: string, measurements: string): string {
+    return `<div class="${htmlClass}">\n${articles}${measurements}</div>`;
 }
 
 function getLastNElements<T>(array: T[], size: number): T[] {
     return array.slice(-size);
+}
+
+function prepareViewElements(viewElements: ViewElements[]): string {
+    if (viewElements.length === 0) {
+        return '';
+    }
+
+    if (instanceOfArticle(viewElements)) {
+        return viewElements.reduce((prevValue, { time, category, title }) => {
+            return prevValue + `[${time}] ${category} - ${title}\n`;
+        }, '');
+    } else if (instanceOfMeasurement(viewElements)) {
+        return viewElements.reduce((prevValue, { time, pressure, humidity, temperature }) => {
+            return prevValue + `[${time}] ${temperature} C, ${pressure} P, ${humidity} U\n`;
+        }, '');
+    } else {
+        throw TypeError('Unknown type');
+    }
+}
+
+function instanceOfArticle(object: any): object is IArticle[] {
+    return (object as IArticle[])[0].category !== undefined;
+}
+
+function instanceOfMeasurement(object: any): object is IMeasurement[] {
+    return (object as IMeasurement[])[0].temperature !== undefined;
 }
 
 export abstract class EffectiveLogView implements IView {
@@ -36,6 +47,11 @@ export abstract class EffectiveLogView implements IView {
 
     private lastMeasurements: IMeasurement[] = [];
     private lastArticles: IArticle[] = [];
+
+    private measurementsView = '';
+    private articlesView = '';
+
+    private changed = false;
 
     public update(observable: IObservable): void {
         if (observable instanceof NewsState) {
@@ -52,20 +68,29 @@ export abstract class EffectiveLogView implements IView {
     }
 
     public render(): void {
-        if (
-            !isEqual(this.currentArticles, this.lastArticles) ||
-            !isEqual(this.currentMeasurements, this.lastMeasurements)
-        ) {
-            const markUp = prepareView(
+        if (!isEqual(this.currentArticles, this.lastArticles)) {
+            this.articlesView = prepareViewElements(this.currentArticles);
+            this.lastArticles = this.currentArticles;
+
+            this.changed = true;
+        }
+
+        if (!isEqual(this.currentMeasurements, this.lastMeasurements)) {
+            this.measurementsView = prepareViewElements(this.currentMeasurements);
+            this.lastMeasurements = this.currentMeasurements;
+
+            this.changed = true;
+        }
+
+        if (this.changed) {
+            const markUp = prepareViewToRender(
                 this.getHtmlClass(),
-                this.currentArticles,
-                this.currentMeasurements
+                this.articlesView,
+                this.measurementsView
             );
 
             console.log(markUp);
-
-            this.lastArticles = this.currentArticles;
-            this.lastMeasurements = this.currentMeasurements;
+            this.changed = false;
         }
     }
 
