7d854e9a79e8237d327db41ecca752d2e3f3a242
nvkvasiliy <vases74@gmail.com>
2019-01-06 15:55:53 +0500

    first try

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -1,12 +1,34 @@
 import { IObservable, IObserver } from '../utils/observable/types';
 import { IView } from './types';
+import { NewsState } from '../state/news';
+import { WeatherState } from '../state/weather';
+import { IArticle } from '../state/news/types';
+import { IMeasurement } from '../state/weather/types';
 
 export class DesktopView implements IObserver, IView {
+    public currentWeather: IMeasurement[] = [];
+    public currentNews: IArticle[] = [];
+
     public update(observable: IObservable) {
-        throw new Error('Not implemented');
+        if (observable instanceof WeatherState) {
+            this.currentWeather = observable.getMeasurements().slice(-2);
+        } else if (observable instanceof NewsState) {
+            this.currentNews = observable.getArticles().slice(-3);
+        }
+        this.render();
     }
 
     public render() {
-        throw new Error('Not implemented');
+        const startOfLog = '<div class="desktop">\n';
+        const endOfLog = '</div>';
+        let contentOfLog = '';
+        this.currentNews.forEach(article => {
+            contentOfLog += `[${article.time}] ${article.category} - ${article.title}\n`;
+        });
+        this.currentWeather.forEach(measurement => {
+            const { time, temperature, pressure, humidity } = measurement;
+            contentOfLog += `[${time}] ${temperature} C, ${pressure} P, ${humidity} U\n`;
+        });
+        console.log(startOfLog + contentOfLog + endOfLog);
     }
 }
--- src/views/types.ts
+++ src/views/types.ts
@@ -1,3 +1,8 @@
+import { IArticle } from '../state/news/types';
+import { IMeasurement } from '../state/weather/types';
+
 export interface IView {
+    currentWeather: IMeasurement[];
+    currentNews: IArticle[];
     render(): void;
 }
