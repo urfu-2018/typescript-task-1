aae610e08fb8dc969aadcd76ca70d33472d658cb
MargaritaNik <mapgosha98@yandex.ru>
2019-01-08 21:52:41 +0500

    first

--- src/utils/observable/index.ts
+++ src/utils/observable/index.ts
@@ -1,15 +1,18 @@
 import { IObservable, IObserver } from './types';
 
 export class Observable implements IObservable {
+    private observers: IObserver[] = [];
     public addObserver(observer: IObserver) {
-        throw new Error('Not implemented');
+        this.observers.push(observer);
     }
 
     public deleteObserver(observer: IObserver) {
-        throw new Error('Not implemented');
+        this.observers.splice(this.observers.indexOf(observer), 1);
     }
 
     public notifyObservers() {
-        throw new Error('Not implemented');
+        this.observers.forEach(element => {
+            element.update(this);
+        });
     }
 }
